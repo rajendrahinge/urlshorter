8de959dc88b36ce2c0431ed3c5c4858b95c38131
rajendrahinge <rajendrahinge24@gmail.com>
2020-12-09 22:33:54 +0530

    changes

---
+++ storage/framework/views/16a37de2b270994b2b452ea10ee81f242c7995aa.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <!-- <link rel="stylesheet" type="text/css" href="<?php echo e(asset('css/app.css')); ?>"> -->
+    </head>
+    <body>
+        <div id="app">
+            <main-component />
+        </div>
+        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+        <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
+        <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+        <script src="https://kit.fontawesome.com/7acce5e64e.js" crossorigin="anonymous"></script>
+    </body>
+</html>
+<?php /**PATH C:\rajendra\xampp7_3\htdocs\urlshortener\resources\views/welcome.blade.php ENDPATH**/ ?>
